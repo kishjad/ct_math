d30b2d70d3a1dcf9b76d88f97d24cc1a587fe62e
Kish Jadhav <kshitej@vt.edu>
2022-01-15 10:19:24 -0500

    Added floor files

---
+++ include/floor.hpp
@@ -0,0 +1,26 @@
+
+#ifndef CT_MATH_FLOOR_HPP
+#define CT_MATH_FLOOR_HPP
+
+namespace floor_operator{
+    template <typename T>
+    constexpr int checkOperator (const T a){
+        return ( ( (a<T(0)) && (T(static_cast<long long int>(a)) != a)) ? 1 : 0);
+    }
+
+    template <typename T>
+    constexpr T floor_check(T a) noexcept{
+        if constexpr(std::is_integral<T>::value){
+            return T(static_cast<long long int>(a));
+        }
+        return T(static_cast<long long int>(a - (checkOperator<T>(a)) ));
+    }
+}
+
+
+template<typename T>
+constexpr auto replace_floor(const T x) -> const T
+{
+    return ct_math::floor_check( static_cast<T>(x) );
+}
+#endif
