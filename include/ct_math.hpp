7d094b12de2f24fa4f49ec23f87a42de4798450e
Kish Jadhav <kshitej@vt.edu>
2022-01-16 13:33:21 -0500

    Enable tests (#2)
    * shoudl convert catch to standalone
    
    * removed catch2
    
    * removed catch2
    
    * udapted catch
    
    * Catch compiles into library
    
    * ctest works
    
    * Testing floor complete

---
+++ include/ct_math.hpp
@@ -0,0 +1,9 @@
+#ifndef CT_MATH_HPP
+#define CT_MATH_HPP
+
+namespace ct_math {
+    #include <helper_functions.hpp>
+    #include <floor.hpp>
+// #include <ceil.hpp>
+}
+#endif
--- include/helper_functions.hpp
+++ include/helper_functions.hpp
@@ -1,8 +1,24 @@
 #ifndef CT_MATH_HELPER_HPP
 #define CT_MATH_HELPER_HPP
 #include <limits>
-
 namespace internal_use {
+
+
+#if __cplusplus >= 202002L
+#include <type_traits>
+#endif
+constexpr bool is_constant_evaluated() {
+#if __cplusplus >= 202002L
+    return std::is_constant_evaluated();
+#elif defined(__GNUC__) // defined for both GCC and clang
+    return __builtin_is_constant_evaluated();
+#else
+    // If the builtin is not available, return a pessimistic result.
+    // This way callers will implement everything in a constexpr way.
+    return true;
+#endif
+}
+
 
 /*-------------------------------------------------------
 Determine if given constexpr value is NaN
@@ -12,7 +28,7 @@ comparisons involving them are always false.
 That is, for a float f, f != f will be true only if f is NaN.
 ---------------------------------------------------------*/
 template <typename T>
-constexpr is_value_nan(const T a) -> const bool{
+constexpr bool is_value_nan(const T a) noexcept{
     return a != a;
 }
 
@@ -20,8 +36,9 @@ constexpr is_value_nan(const T a) -> const bool{
 Determine if given constexpr value is infinite
 Check value against +/- of inbuilt infinity function 
 ---------------------------------------------------------*/
-constexpr is_value_inf(const T a) -> const bool{
-    return ( (a != -std::numeric_limits<T>::infinity()) && (a != std::numeric_limits<T>::infinity()) )
+template <typename T>
+constexpr bool is_value_inf(const T a) noexcept{
+    return ( (a == -std::numeric_limits<T>::infinity()) && (a == std::numeric_limits<T>::infinity()) );
 }
 
 };
